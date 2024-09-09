1002060529add3d4d714149d9611c5efd8cd39e3
meenagiri18 <meenagiri2058@gmail.com>
2024-09-09 22:19:39 +0545

    formerror fixed

--- backend/Front/frontend/src/component/route.js
+++ backend/Front/frontend/src/component/route.js
@@ -1,4 +1,4 @@
-import React,{ useState,useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
 import './route.css'
 import Navbar from './navbar'
 import axios from "axios";
@@ -7,67 +7,75 @@ export default function RouteForm() {
   const [flocation, setFlocation] = useState('');
   const [tlocation, setTlocation] = useState('');
   const [shortestPath, setShortestPath] = useState(null);
-  const [data, setData] = useState([]);
 
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log('Form submitted with:', { flocation, tlocation });
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   console.log('Form submitted with:', { flocation, tlocation });
+
+  //   try {
+  //     const response = await axios.post('http://127.0.0.1:8000/route/', {
+  //       flocation,
+  //       tlocation
+  //     });
+
+
+  //     const { shortestPath } = response.data;
+
+  //     setShortestPath(shortestPath);
+  //   } catch (error) {
+  //     console.error('Error:', error.response?.data?.error || error.message);
+  //   }
+  // };
+  useEffect(() => {
+    axios.get('').then((response) =>{
+      setShortestPath(response.data);
+
+
+    })
+    .catch(console.log('Error'))
+   
   
-    try {
-      const response = await axios.post('http://127.0.0.1:8000/route/', {
-        flocation,
-        tlocation
-      });
+    
+  }, [])
   
-      // Manual JSON decoding (though unnecessary in this case)
-      const jsonData = JSON.parse(JSON.stringify(response.data));
-      const { shortestPath } = jsonData;
-      setShortestPath(shortestPath);
-    } catch (error) {
-      console.error('Error:', error.response?.data?.error || error.message);
-    }
-  };
-  // useEffect(() => {
-  //   axios.get('http://127.0.0.1:8000/api/calculate_route/').then((response)=>{
-  //     setData(response.data);
-  //     console.log(response.data)
-  //   })
-                        
-  // }, [])
-  
+
+
 
   return (
     <div>
-        <div><Navbar/></div>
-        <div className='main-container2'>
-            <div className='main2'>
-            
-      <form onSubmit={handleSubmit} >
-        <div className='design3'>
-            <div className='field'>
-                <label htmlFor='flocation'>From location</label>
-                <input name = "Flocation" id='flocation' type='text'value={flocation}  onChange={(e) => setFlocation(e.target.value)} required></input>
-            </div>
-            <div className='field'>
-                <label htmlFor='tlocation'>To location</label>
-                <input name = "Tlocation" id='tlocation' type='text'value={tlocation}
-                  onChange={(e) => setTlocation(e.target.value)} required></input>
-            </div>
-        </div>
-        <div>
-            <button type='submit'>Check</button>
-        </div>
-        
-      </form>
+      <div><Navbar /></div>
+      <div className='main-container2'>
+        <div className='main2'>
 
-      {/* {shortestPath && (
+          <form method="POST" action='' >
+            <div className='design3'>
+              <div className='field'>
+                <label htmlFor='flocation'>From location</label>
+                <input name="Flocation" id='flocation' type='text' value={flocation} 
+                onChange={(e) => setFlocation(e.target.value)} required></input>
+              </div>
+
+              <div className='field'>
+                <label htmlFor='tlocation'>To location</label>
+                <input name="Tlocation" id='tlocation' type='text' value={tlocation}
+                  onChange={(e) => setTlocation(e.target.value)} required></input>
+              </div>
+            </div>
+
+            <div>
+              <button type='submit'>Check</button>
+            </div>
+
+          </form>
+
+          {shortestPath && (
             <div className="result">
               <h3>Shortest Path: {shortestPath} km</h3>
             </div>
-          )} */}
+          )}
 
-      </div>
+        </div>
       </div>
     </div>
   )
